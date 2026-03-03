b96d996bade3a1587691fb2230b23211fb4ade75
Daniel Szalaty <dusherr@o2.pl>
2026-03-03 13:46:38 +0000

    configured translations using i18next with locize backend

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,13 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { useState } from "react";
+import reactLogo from "./assets/react.svg";
+import viteLogo from "/vite.svg";
+import "./App.css";
+import { useTranslation, Trans } from "react-i18next";
+import { Suspense } from "react";
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [count, setCount] = useState(0);
+  const { t, i18n } = useTranslation();
 
   return (
     <>
@@ -22,14 +25,23 @@ function App() {
           count is {count}
         </button>
         <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
+          <Trans i18nKey="editCode">
+            Edit <code>src/App.tsx</code> and save to test HMR
+          </Trans>
         </p>
       </div>
+      <p>foo</p>
       <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
+        {t("clickLogos", "Click on the Vite and React logos to learn more")}
       </p>
     </>
-  )
+  );
 }
 
-export default App
+export default function WrappedApp() {
+  return (
+    <Suspense fallback="...is loading">
+      <App />
+    </Suspense>
+  );
+}
---
+++ src/i18n.tsx
@@ -0,0 +1,46 @@
+import i18n from "i18next";
+import { initReactI18next } from "react-i18next";
+import LanguageDetector from "i18next-browser-languagedetector";
+import LocizeBackend from "i18next-locize-backend";
+import LastUsed from "locize-lastused";
+import { locizePlugin } from "locize";
+
+const isDev = import.meta.env.DEV;
+
+const locizeOptions = {
+  projectId: import.meta.env.VITE_LOCIZE_PROJECTID,
+  apiKey: import.meta.env.VITE_LOCIZE_APIKEY, // YOU should not expose your app's API key to production!
+  referenceLng: "en",
+};
+
+if (isDev) {
+  // locize-lastused
+  // sets a timestamp of last access on every translation segment on locize
+  // -> safely remove the ones not being touched for weeks/months
+  // https://github.com/locize/locize-lastused
+  i18n.use(LastUsed);
+}
+
+i18n
+  // i18next-locize-backend
+  // loads translations from your project, saves new keys to it (saveMissing: true)
+  // https://github.com/locize/i18next-locize-backend
+  .use(LocizeBackend)
+  // detect user language
+  // learn more: https://github.com/i18next/i18next-browser-languageDetector
+  .use(LanguageDetector)
+  // Bind i18next to React
+  .use(initReactI18next)
+  // InContext Editor of locize
+  .use(locizePlugin)
+  // init i18next
+  // for all options read: https://www.i18next.com/overview/configuration-options
+  .init({
+    debug: isDev,
+    fallbackLng: "en",
+    backend: locizeOptions,
+    locizeLastUsed: locizeOptions,
+    saveMissing: isDev, // you should not use saveMissing in production
+  });
+
+export default i18n;
